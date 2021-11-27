deb55a4bff75ec3055a4de4e28332ea038a8af71
Atul Varma <varmaa@gmail.com>
2021-11-26 21:02:28 -0500

    Add 'take photo' functionality.

--- app/src/main/java/com/toolness/jankshot/MainActivity.kt
+++ app/src/main/java/com/toolness/jankshot/MainActivity.kt
@@ -2,28 +2,36 @@ package com.toolness.jankshot
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.toolness.jankshot.databinding.ActivityMainBinding
+import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private var imageCapture: ImageCapture? = null
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var outputDirectory: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        outputDirectory = getOutputDirectory()
 
         if (allPermissionsGranted()) {
             startCamera()
@@ -32,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+        binding.takePhoto.setOnClickListener {
+            takePhoto()
+        }
     }
 
     override fun onDestroy() {
@@ -69,17 +81,42 @@ class MainActivity : AppCompatActivity() {
                 it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
             }
 
+            imageCapture = ImageCapture.Builder().build()
+
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
                 cameraProvider.unbindAll()
 
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
             } catch(exc: Exception) {
+                // Apparently this can happen if e.g. the app is no longer focused when our callback is called.
                 Log.e(TAG, "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(this))
         Toast.makeText(this, "Jankshot is starting!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(outputDirectory, "jankshot.jpg")
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exc.message}")
+            }
+
+            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo capture succeeded: ${savedUri}"
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+        })
+    }
+
+    private fun getOutputDirectory(): File {
+        return filesDir
     }
 
     companion object {
